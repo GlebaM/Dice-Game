b39e342d655ad53ccf9a2fc4a634d71ba299a652
Michał Gleba <GlebaM@users.noreply.github.com>
2021-06-07 15:09:23 +0200

    Start

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+// Element labells
+const name0El = document.querySelector('#name--0');
+const name1El = document.getElementById('name--1');
+// Players current scores
+const current0El = document.querySelector('#current--0');
+const current1El = document.getElementById('current--1');
+
+//Initial players scores
+score0El.textContent = 0;
+score1El.textContent = 0;
